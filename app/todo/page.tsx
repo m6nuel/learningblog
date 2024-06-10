06427de36a695e7b8d5b6ffbb4a90ed24048a3e1
Jesus M Delgado D <m6nuel@gmail.com>
2024-06-10 15:55:13 -0300

    nuevas tareas

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -2,10 +2,10 @@ const page = () => {
   return (
     <div>
       <h5>- Acomodar estilos</h5>
-      <h5>- Login</h5>
+      <h5>- Usar DB Para cargar post</h5>
       <h5>- Hacer Carga de Post</h5>
-      <h5>- Buscar Imagen para Imagen del User</h5>
-      <h5>- Hacer Boton de login</h5>
+      <h5>- Proteger Rutas</h5>
+      <h5>- Acomodar el dashboard</h5>
       <h5>- Centrar el NavBar</h5>
       <h5>https://www.youtube.com/watch?v=jMy4pVZMyLM&t=3435s min 1:03:00</h5>
       <h5>Solucionar el error de navlink</h5>
